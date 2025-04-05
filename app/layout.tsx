0d8aefe92cf55b3f3553ee6a7bf6ab26e0f29daf
Ncassidy10 <nathan@formaxco.com>
2025-04-04 22:27:26 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
+// app/layout.tsx
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <main>{children}</main>
+      </body>
     </html>
-  )
+  );
 }
