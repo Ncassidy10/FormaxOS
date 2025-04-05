704a51dba83a62cfc7f0fac64bec3f51ae9e856c
Ncassidy10 <nathan@formaxco.com>
2025-04-04 22:24:53 -0400

    Add files via upload

---
+++ app/executive/page.tsx
@@ -0,0 +1,8 @@
+export default function ExecutiveDashboard() {
+  return (
+    <div>
+      <h1>Executive Dashboard</h1>
+      <p>This is where executive KPIs and quarterly goals will live.</p>
+    </div>
+  );
+}
